1352c541dccfbc82eb5aae03659fca149a0d43ea
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-21 05:33:31 -0500

    Update constructors.jl

--- test/constructors.jl
+++ test/constructors.jl
@@ -1,24 +1,34 @@
+const one32 = one(Float32)
+const one64 = one(Float64)
+
 @testset "Constructors" begin
-    @test Double(2.0) isa Double{Float64, Fast}
-    @test Double(2.0, Accurate) isa Double{Float64, Accurate}
-    @test Double{Float64}(2.0) isa Double{Float64, Fast}
-    @test Double(2.0, Accurate) isa Double{Float64, Accurate}
-    @test Double(2.0f0) isa Double{Float32, Fast}
-    @test Double(2.0f0, Accurate) isa Double{Float32, Accurate}
-    @test Double{Float64}(2.0f0) isa Double{Float64, Fast}
-    @test Double{Float64}(2.0f0, Accurate) isa Double{Float64, Accurate}
-    @test Double(π, Fast) isa Double{Float64, Fast}
-    @test Double(π, Accurate) isa Double{Float64, Accurate}
-    @test Double{Float32}(π, Fast) isa Double{Float32, Fast}
-    @test Double{Float32}(π, Accurate) isa Double{Float32, Accurate}
-    @test Double(big(π), Fast) isa Double{Float64, Fast}
-    @test Double(big(π), Accurate) isa Double{Float64, Accurate}
-    @test Double{Float32}(rand(), Fast) isa Double{Float32, Fast}
-    @test Double{Float32}(rand(), Accurate) isa Double{Float32, Accurate}
-    @test Double{Float32}(big(π), Fast) isa Double{Float32, Fast}
-    @test Double{Float32}(big(π), Accurate) isa Double{Float32, Accurate}
-    @test Double(1//3, Fast) isa Double{Float64, Fast}
-    @test Double(1//3, Accurate) isa Double{Float64, Accurate}
-    @test Double{Float32}(1//3, Fast) isa Double{Float32, Fast}
-    @test Double{Float32}(1//3, Accurate) isa Double{Float32, Accurate}
+    @test Double(one64) isa Double{Float64, EMPHASIS}
+    @test Double{Float64}(1) isa Double{Float64, EMPHASIS}
+    @test Double(1) isa Double{Float64, EMPHASIS}
+    @test Double(1.0) isa Double{Float64, EMPHASIS}
+    @test Double(Accuracy, 1.0) isa Double{Float64, Accuracy}
+    @test Double(Performance, 1.0) isa Double{Float64, Performance}
+    @test Double(Accuracy, one64) isa Double{Float64, Accuracy}
+    @test Double(Performance, one64) isa Double{Float64, Performance}
+
+    @test Double(one32) isa Double{Float32, EMPHASIS}
+    @test Double{Float32}(1) isa Double{Float32, EMPHASIS}
+    @test Double(Accuracy, 1.0f0) isa Double{Float32, Accuracy}
+    @test Double(Performance, 1.0f0) isa Double{Float32, Performance}
+    @test Double(1.0f0) isa Double{Float32, EMPHASIS}
+    @test Double(Accuracy, one32) isa Double{Float32, Accuracy}
+    @test Double(Performance, one32) isa Double{Float32, Performance}
+
+    @test Double(π, Performance) isa Double{Float64, Performance}
+    @test Double(π, Accuracy) isa Double{Float64, Accuracy}
+    @test Double{Float32}(π, Performance) isa Double{Float32, Performance}
+    @test Double{Float32}(π, Accuracy) isa Double{Float32, Accuracy}
+    @test Double(big(π), Performance) isa Double{Float64, Performance}
+    @test Double(big(π), Accuracy) isa Double{Float64, Accuracy}
+    @test Double{Float32}(big(π), Performance) isa Double{Float32, Performance}
+    @test Double{Float32}(big(π), Accuracy) isa Double{Float32, Accuracy}
+    @test Double(1//3, Performance) isa Double{Float64, Performance}
+    @test Double(1//3, Accuracy) isa Double{Float64, Accuracy}
+    @test Double{Float32}(1//3, Performance) isa Double{Float32, Performance}
+    @test Double{Float32}(1//3, Accuracy) isa Double{Float32, Accuracy}
 end
