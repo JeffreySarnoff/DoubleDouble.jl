8b4dcafbf57662024489765e5ec4c9ddc6018f03
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-21 08:33:10 -0500

    Update convert.jl

--- src/convert.jl
+++ src/convert.jl
@@ -1,3 +1,5 @@
+
+
 import Base: promote_rule, convert
 
 promote_rule(::Type{Double{T,Accuracy}}, ::Type{Double{T,Performance}}) where T<:SysFloat = Double{T,Accuracy}
@@ -11,6 +13,16 @@ promote_rule(::Type{Double{T,E}}, ::Type{Rational{I}}) where {I<:Integer, T<:Sys
 promote_rule(::Type{Double{T,E}}, ::Type{BigInt}) where {T<:SysFloat, E<:Emphasis} = Double{Float64,E}
 promote_rule(::Type{Double{T,E}}, ::Type{BigFloat}) where {T<:SysFloat, E<:Emphasis} = Double{Float64,E}
 promote_rule(::Type{Double{T,E}}, ::Type{R}) where {R<:Real, T<:SysFloat, E<:Emphasis} = Double{Float64,E}
+
+Base.convert(::Type{Float64}, x::Double{Float64, E}) where E<:Emphasis = x.hi
+Base.convert(::Type{Float64}, x::Double{Float32, E}) where E<:Emphasis = Float64(x.hi)
+Base.convert(::Type{Float64}, x::Double{Float16, E}) where E<:Emphasis = Float64(x.hi)
+Base.convert(::Type{Float32}, x::Double{Float64, E}) where E<:Emphasis = Float32(x.hi)
+Base.convert(::Type{Float32}, x::Double{Float32, E}) where E<:Emphasis = x.hi
+Base.convert(::Type{Float32}, x::Double{Float16, E}) where E<:Emphasis = Float32(x.hi)
+Base.convert(::Type{Float16}, x::Double{Float64, E}) where E<:Emphasis = Float16(x.hi)
+Base.convert(::Type{Float16}, x::Double{Float32, E}) where E<:Emphasis = Float16(x.hi)
+Base.convert(::Type{Float16}, x::Double{Float16, E}) where E<:Emphasis = x.hi
 
 convert(::Type{Double{T,Accuracy}}, x::Double{T,Performance}) where T<:SysFloat = Double(Accuracy, x.hi, x.lo)
 convert(::Type{Double{T,Performance}}, x::Double{T,Accuracy}) where T<:SysFloat = Double(Performance, x.hi, x.lo)
@@ -64,4 +76,5 @@ function convert(::Type{R}, x::Double{T,E}) where {R<:Real, T<:SysFloat, E<:Emph
    bf = convert(BigFloat, x)
    return round(R, bf)
 end
+
 
