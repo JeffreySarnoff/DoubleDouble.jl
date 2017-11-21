525c8d3b96951ce070f4cf6760ecdbbb686a15aa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-21 08:00:12 -0500

    Update DoubleDouble.jl

--- src/DoubleDouble.jl
+++ src/DoubleDouble.jl
@@ -4,6 +4,8 @@ module DoubleDouble
 
 export Double, FastDouble
 
+
+
 const SysFloat = Union{Float16, Float32, Float64}
 
 # Algorithmic choice is a Trait
@@ -25,26 +27,41 @@ struct Double{T<:SysFloat, E<:Emphasis} <: AbstractDouble{T}
     lo::T
 end
 
-function Double(::Type{E}, hi::T) where {T<:SysFloat, E<:Emphasis}
-    return Double{T,E}(hi, zero(T))
-end
-
 function Double(::Type{E}, hi::T, lo::T) where {T<:SysFloat, E<:Emphasis}
     s = hi + lo
     e = (hi - s) + lo
     return Double{T,E}(s, e)
 end
 
-Double(x)      = Double(Accuracy, x)
-Double(x, y)   = Double(Accuracy, x, y)
+function Double(::Type{E}, hi::T) where {T<:SysFloat, E<:Emphasis}
+    return Double{T,E}(hi, zero(T))
+end
 
-FastDouble(x)    = Double(Performance, x)
-FastDouble(x, y) = Double(Performance, x, y)
+function Double(::Type{E}, hi::T, lo::T) where {T<:Signed, E<:Emphasis}
+    return Double(E, float(hi), float(lo))
+end
+function Double(::Type{E}, hi::T) where {T<:Signed, E<:Emphasis}
+    return Double(E, float(hi), float(zero(T)))
+end
 
-@inline Double(hi::T, lo::T) where T<:SysFloat = Double(EMPHASIS, hi, lo)
-@inline Double(x::T) where T<:SysFloat = Double(x, zero(T))
-@inline Double(x::T) where T<:Real = Double(Float64(x))
-@inline Double(x::T1, y::T2) where {T1<:Real, T2<:Real} = Double(Float64(x), Float64(y))
+function Double(::Type{E}, hi::T, lo::T) where {T<:Real, E<:Emphasis}
+    s = Float64(hi + lo)
+    e = Float64((hi - T(s)) + lo)
+    return Double{Float64,E}(s,e)
+end
+function Double(::Type{E}, hi::T) where {T<:Real, E<:Emphasis}
+    s = Float64(hi)
+    e = Float64(hi - T(s))
+    return Double{Float64,E}(s,e)
+end
+
+Double(x::S) where S<:SysFloat = Double(Accuracy, x)
+Double(x::R) where R<:Real = Double(Accuracy, x)
+Double(x::S, y::S) where S<:SysFloat = Double(Accuracy, x)
+Double(x::R, y::R) where R<:Real = Double(Accuracy, x, y)
+
+FastDouble(x::R) where R<:Real       = Double(Performance, x)
+FastDouble(x::R, y::R) where R<:Real = Double(Performance, x, y)
 
 include("convert.jl")
 include("compare.jl")
