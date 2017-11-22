f15eb35186f789f7a45852535e2e4e02d483f300
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-11-22 05:42:10 -0500

    Update arith.jl

--- src/arith.jl
+++ src/arith.jl
@@ -1,6 +1,6 @@
 import Base: (+), (-), (*), (/), div, fld, cld, rem, mod, sqrt
 
-function (+)(a::Double{T,E}, b::T) where {T,E}
+function (+)(a::Double{T,E}, b::T) where {T<:SysFloat,E<:Emphasis}
     hi, lo = two_sum(a.hi, b)
     lo += a.lo
     hi, lo = two_sum_ordered(hi, lo)
@@ -8,7 +8,7 @@ function (+)(a::Double{T,E}, b::T) where {T,E}
     return Double(E, hi, lo)
 end
 
-function (+)(a::T, b::Double{T,E}) where {T,E}
+function (+)(a::T, b::Double{T,E}) where {T<:SysFloat,E<:Emphasis}
     hi, lo = two_sum(a, b.hi)
     lo += b.lo
     hi, lo = two_sum_ordered(hi, lo)
@@ -23,21 +23,23 @@ function (+)(a::Double{T,E}, b::Double{T,E}) where {T,E}
     hi = hihi + hilo
     hilo -= hi - hihi
     lo += hilo
-    hi,lo = two_sum(hi, o)
+    hi,lo = two_sum(hi, lo)
     return Double(E, hi, lo)
 end
 
 
 function (add)(a::Double{T,E}, b::Double{T,E}) where {T,E}
-    hihi, hilo = two_sum(a.hi, b.hi)
-    lohi, lolo = two_sum(a.lo, b.lo)
-    hilo += lohi
-    lohi = hihi + hilo
-    hilo -= lohi - hihi
-    hilo += lolo
-    hi,lo = two_sum(lohi,hilo)
-    return Double(E, hi, lo)
+    hiab, abhi = two_sum(a.hi, b.hi)
+    loab, ablo = two_sum(a.lo, b.lo)
+    abhi += loab
+    loab = hiab + abhi
+    abhi -= loab - hiab
+    ablo += abhi
+    loab,ablo = two_sum(loab,ablo)
+    return Double(E, loab, ablo)
 end
+
+
 
     
 +(a::T, b::Double{T,E}) where {T<:SysFloat, E<:Emphasis} = b + a
